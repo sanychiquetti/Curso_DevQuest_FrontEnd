626b30f8de818ddc9634afc81af69ac51bb54b27
sanychiquetti <sanymara77@hotmail.com>
2023-03-02 17:16:34 -0300

    alert, prompt, confimr, variáveis...

---
+++ 7-javascript-basico/1-primeiro-codigo-javascript/index.js
@@ -0,0 +1,11 @@
+//prompt('Qual sua idade?')
+ 
+// agora podemos armazenar em variáveis:
+let retornoDoUsuario = confirm('Deseja continuar?')
+//com o console.log conseguimos vem no DevToos o que o usuário optou
+console.log(retornoDoUsuario);
+// se ele clicou em ok, vai aparecer true, se clicou em cancelar vai aparecer false
+
+
+let idade = prompt('Digite sua idade')
+console.log(idade)
