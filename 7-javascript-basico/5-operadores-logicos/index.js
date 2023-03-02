e437bc23587e27257c1c23ed22b5d07cc4ae5d2e
sanychiquetti <sanymara77@hotmail.com>
2023-03-02 19:41:54 -0300

    Tipos primitivos e operadores lógicos

---
+++ 7-javascript-basico/5-operadores-logicos/index.js
@@ -0,0 +1,45 @@
+/*  
+==  ”Igual”, sempre dois para comparar valores;
+a == b = true  - leia-se: Se a for igual ao b, recebe verdadeiro (true)
+const a = 3 //(a recebe 3)
+const b = 3 //(b recebe 3)
+console.log(a == b); 
+
+
+=== “Idêntico” sempre usar 3 sinais de =;   
+a === b = true  → leia-se: Se a for idêntico ao b, recebe verdadeiro (true)
+const a = 1
+const b = 1
+console.log(a === b);
+
+
+!== Não Idêntico : é igual ao idêntico, mas com uma negação na frente;
+a !== b = true  → leia-se: Se a não for idêntico ao b, recebe verdadeiro (true)
+const a = 2
+const b = 2
+console.log(a !== b); 
+
+
+!= Diferente: para ver se são doferentes;
+a != b -> leia-se: Se a é diferente de b
+const a = 4
+const b = 4
+console.log(a != b);
+
+< Menor que : sinal de menor;
+a < b = true  → leia-se: Se a for menor que b, recebe verdadeio (true) 
+const a = 4
+const b = 4
+console.log(a < b); 
+
+> Maior que : sinal de maior;
+a > b = true  → leia-se: Se a for maior que b, recebe verdadeio (true)
+const a = 4
+const b = 4
+console.log(a > b);
+
+Nos dois casos acima < e > posso usar o sinal de maior ou igual ex:
+a <= b -> Leia-se a é menor ou igual a b
+ou
+a >= b -> Leia-se a é maior ou igual a b
+*/
