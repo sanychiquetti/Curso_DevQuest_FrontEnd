06c9b87baca8eefb87606dc1fd07e6e75405d62c
sanychiquetti <sanymara77@hotmail.com>
2023-09-03 19:39:48 -0300

    Aprendendo sobre arrow functions

---
+++ 8_JS_Intermediario/4-arrow-function/index.js
@@ -0,0 +1,21 @@
+/*
+let somar = () => {
+   return numero1 + numero2
+} 
+console.log(somar(15, 6))
+
+quando temo apenas uma linha de código, podemos simplificar nossa função
+let somar = (numero1, numero2) => numero1 + numero2
+console.log(somar(15, 6))
+
+Exemplo 2:
+const incentivarQuester = nomeQuester => {
+   console.log('Parabens ' + nomeQuester + ', Você chegou ao módulo de JS Intermediário')
+} 
+incentivarQuester('Sany')
+Forma simplificada:
+*/
+const incentivarQuester = nomeQuester => 
+   console.log('Parabens ' + nomeQuester + ', Você chegou ao módulo de JS Intermediário')
+
+incentivarQuester('Sany')
